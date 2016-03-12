694a2b118314fb6e6289bca9ce9324f824ded611
shaikrishna <shaikrishna@10.0.0.7>
2016-03-11 17:52:06 -0800

    adding test1

---
+++ src/method_vs_variable31/Test1.java
@@ -0,0 +1,16 @@
+package method_vs_variable31;
+//if application contain  instance variable the priority goes instance variable//
+public class Test1 {
+	int a=100;
+	int m1(){
+		System.out.println("m1 method");
+		return a;
+	}
+
+	public static void main(String[] args) {
+		Test1 t1=new Test1();
+		int x=t1.m1();
+      System.out.println(x);
+	}
+
+}
