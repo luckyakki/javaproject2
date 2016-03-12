39bcf03bbd057096cf3b301a9fa22797961d393b
shaikrishna <shaikrishna@10.0.0.7>
2016-03-11 17:39:53 -0800

    UNCHECKED

---
+++ src/exception32/Uncheckedexception.java
@@ -0,0 +1,15 @@
+package exception32;                              //javabyratan//
+
+public class Uncheckedexception {
+	
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println(10/0);
+		System.out.println("hi");
+		System.out.println("hi");
+
+	}
+
+}
