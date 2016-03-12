93993751d699bf3dd717928cee922402c3e9a60f
shaikrishna <shaikrishna@10.0.0.7>
2016-03-11 17:37:24 -0800

    UNCHECKED

---
+++ src/exception32/UncheckedEcception3.java
@@ -0,0 +1,13 @@
+package exception32;
+
+public class UncheckedEcception3 {
+
+	public static void main(String[] args) {
+		String str ="sappu";
+		System.out.println(str.charAt(2));
+		System.out.println(str.charAt(3));
+        //System.out.println(str.charAt(8));//
+	
+	
+}
+}
