a552acc4390d685524072e0d9f33340738fe9aa1
shaikrishna <shaikrishna@10.0.0.7>
2016-03-11 17:41:51 -0800

    UNCHECKED1

---
+++ src/exception32/UncheckedException1.java
@@ -0,0 +1,18 @@
+package exception32;
+
+public class UncheckedException1 {
+	
+	
+	public static void main(String args[]){
+		int num[]={1,2,3,4,5};
+		System.out.println(num[0]);
+		System.out.println(num[1]);
+		System.out.println(num[2]);
+		System.out.println(num[3]);
+		System.out.println(num[5]);
+		System.out.println(num[4]);
+		System.out.println(num[1]);
+		
+	}
+
+}
